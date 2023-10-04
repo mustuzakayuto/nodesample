491040445b9168f11e1de2a76afe35a895da1581
松坂侑隼 <220063ie@yse-c.net>
2023-10-04 11:54:53 +0900

    server

---
+++ express_sample/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+console.log(HOST)
+console.log(PORT)
+
+const app = express()
+app.get("/",(req,res)=>{
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    res.send("Hello!!!!!!!")
+})
+
+app.listen(PORT,HOST,()=>{
+    console.log(HOST)
+    console.log(PORT)
+    console.log("wait...")
+})
